d3987356f2cd340fd2690042243d67622ed92b0e
NatashaLaurie <71662387+NatashaLaurie@users.noreply.github.com>
2021-03-29 14:47:15 +0300

    init: add simple-chain task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  const {
+    repeatTimes = 1,
+    separator = "+",
+    addition = "",
+    additionRepeatTimes = 1,
+    additionSeparator = "|",
+  } = options;
+  let string;
+  if (typeof str != 'string') {
+    string = `${str}`;
+  } else string =  str;
+  const result = [];
+  let i = 0;
+  while (i < repeatTimes) {
+    let subStr = string;
+    if (addition !== "") {
+      let addOptions = {
+        repeatTimes: additionRepeatTimes,
+        separator: additionSeparator,
+      };
+      subStr += repeater(addition, addOptions);
+    }
+    result.push(subStr);
+    i++;
+  };
+
+  return result.join(separator);
 };
   
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof(value) !== 'string') value = `${value}`;
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof(position) !== 'number' || position !== (position | 0) || !(position - 1 in this.chain)) {
+      this.chain.length = 0;
+      throw new Error('THROWN');
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let str = '';
+    if (this.chain.length) {
+      str = `( ${this.chain.join(' )~~( ')} )`;
+      this.chain.length = 0;
+    }
+    return str;
+  },
 };
 
 module.exports = chainMaker;
